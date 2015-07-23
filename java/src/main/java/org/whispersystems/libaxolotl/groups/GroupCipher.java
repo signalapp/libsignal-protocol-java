303ede269ea93378320355070d1f63a754ae6f9f
Ehren Kret <ehren.kret@gmail.com>
2015-07-23 09:36:37 -0700

    Fix sign issue in group cipher

--- java/src/main/java/org/whispersystems/libaxolotl/groups/GroupCipher.java
+++ java/src/main/java/org/whispersystems/libaxolotl/groups/GroupCipher.java
@@ -168,7 +168,7 @@ public class GroupCipher {
       }
     }
 
-    if (senderChainKey.getIteration() - iteration > 2000) {
+    if (iteration - senderChainKey.getIteration() > 2000) {
       throw new InvalidMessageException("Over 2000 messages into the future!");
     }
 
