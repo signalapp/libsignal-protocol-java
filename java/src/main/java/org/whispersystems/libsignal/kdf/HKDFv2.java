803dc5ba3a83022849f399d6af4dcd6a6badd7de
Moxie Marlinspike <moxie@thoughtcrime.org>
2016-05-02 13:20:09 -0700

    Update headers, add LICENSE file.
    Fixes #22
    Closes #23
    
    // FREEBIE

--- java/src/main/java/org/whispersystems/libsignal/kdf/HKDFv2.java
+++ java/src/main/java/org/whispersystems/libsignal/kdf/HKDFv2.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) 2014-2016 Open Whisper Systems
+ *
+ * Licensed according to the LICENSE file in this repository.
+ */
 package org.whispersystems.libsignal.kdf;
 
 public class HKDFv2 extends HKDF {
--- java/src/main/java/org/whispersystems/libsignal/state/impl/InMemorySignedPreKeyStore.java
+++ java/src/main/java/org/whispersystems/libsignal/state/impl/InMemorySignedPreKeyStore.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright (C) 2014-2016 Open Whisper Systems
+ *
+ * Licensed according to the LICENSE file in this repository.
+ */
 package org.whispersystems.libsignal.state.impl;
 
 import org.whispersystems.libsignal.InvalidKeyIdException;
